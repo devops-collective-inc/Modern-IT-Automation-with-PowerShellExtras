fcf92c7698d4be953c61a3123bc3c39bb30ee366
Bill Kindle <billkindle@users.noreply.github.com>
2022-09-17 10:21:03 -0400

    Update Example 9.ps1
    added comments to show console history can be opened with other apps capable of reading plain text.

--- Edition-01/Examples/Logging/Example 9.ps1
+++ Edition-01/Examples/Logging/Example 9.ps1
@@ -1,1 +1,5 @@
+# Will open console history with Visual Studio Code, if installed
 code (Get-PSReadlineOption).HistorySavePath
+
+# Alternatively, any app that can open plain text will work:
+# notepad (Get-PSReadlineOption).HistorySavePath
